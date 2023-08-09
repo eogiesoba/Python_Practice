d98cbf8148c0ac3cb2dfb85442be69f9b4304887
eogiesoba <eogiesoba@gmail.com>
2023-08-08 23:55:59 -0500

    Account for those numbers that are bigger than 23

--- first.py
+++ first.py
@@ -41,4 +41,7 @@ current_time_int = int(current_time_str)
 wait_time_int = int(wait_time_str)
 
 final_time_int = current_time_int + wait_time_int
+
+final_answer = final_time_int % 24
+
 print(final_time_int)
